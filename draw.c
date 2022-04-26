b9e51db04fbdc5e602cb4be0b38ae7cc9919d63c
Dandya <70721037+Dandya@users.noreply.github.com>
2022-04-26 13:45:13 +0300

    update draw.c

--- draw.c
+++ draw.c
@@ -4,11 +4,13 @@ void draw_area(uint32_t *ptr, int xres, int yres, int scr_xres, int scr_yres)
 {
   for(int i = 0; i<xres+2; i++)
   {
-      ptr[i] = ptr[i+(yres+1)*scr_xres] = WHITE;
+      ptr[i] = WHITE;
+      ptr[i+(yres+1)*scr_xres] = WHITE;
   }
   for(int i = 1; i<=yres; i++)
   {
-      ptr[i*scr_xres] = ptr[i*scr_xres+(xres+1)] = WHITE;
+      ptr[i*scr_xres] = WHITE;
+      ptr[i*scr_xres+(xres+1)] = WHITE;
   }
 }
 
@@ -19,6 +21,7 @@ int draw_car(uint32_t* ptr, char direction, uint32_t color, int src_xres)
         case UP:
             for(int i = 0; i>-8; i--)
             {
+                for(int j)
                 ptr[-2+i*src_xres]=ptr[]
             }
             break;
