e0e0dc8c8371bf97e9b5b5315fc2922b7135e01d
Dandya <70721037+Dandya@users.noreply.github.com>
2022-04-26 09:44:46 +0300

    update draw.c

--- draw.c
+++ draw.c
@@ -4,13 +4,11 @@ void draw_area(uint32_t *ptr, int xres, int yres, int scr_xres, int scr_yres)
 {
   for(int i = 0; i<xres+2; i++)
   {
-      ptr[i] = 0x00FFFFFF;
-      ptr[i+(yres+1)*scr_xres] = 0x00FFFFFF;
+      ptr[i] = ptr[i+(yres+1)*scr_xres] = WHITE;
   }
   for(int i = 1; i<=yres; i++)
   {
-      ptr[i*scr_xres] = 0x00FFFFFF;
-      ptr[i*scr_xres+(xres+1)] = 0x00FFFFFF;
+      ptr[i*scr_xres] = ptr[i*scr_xres+(xres+1)] = WHITE;
   }
 }
 
