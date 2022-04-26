0a9eeeed45cd4d002ba2606547a8126098c0027b
Dandya <70721037+Dandya@users.noreply.github.com>
2022-04-26 13:55:17 +0300

    update draw.c

--- draw.c
+++ draw.c
@@ -16,12 +16,17 @@ void draw_area(uint32_t *ptr, int xres, int yres, int scr_xres, int scr_yres)
 
 int draw_car(uint32_t* ptr, char direction, uint32_t color, int src_xres)
 {
+    int result = 1;
     switch (direction)
     {
         case UP:
             for(int i = 0; i>-8; i--)
             {
-                if( (ptr[-2+i*src_xres]+=
+                if( (ptr[-2+i*src_xres]+=color) != color ||
+                    (ptr[-1+i*src_xres]+=color) != color ||
+                    (ptr[i*src_xres]+=color) != color ||
+                    (ptr[1+i*src_xres]+=color) != color ||
+                    (ptr[-2+i*src_xres]+=color) != color
                     )
                 
             }
